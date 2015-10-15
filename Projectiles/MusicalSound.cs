fc20fc5f1fb18ce66f63f5d7de34ea076a0be1a7
napen123 <napen123@gmail.com>
2015-10-14 19:42:49 -0600

    Got started on the projectile

--- Projectiles/MusicalSound.cs
+++ Projectiles/MusicalSound.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Terraria;
+using Terraria.ModLoader;
 
 namespace TheMusicalMod.Projectiles
 {
-    class MusicalProjectile
+    public class MusicalSound : ModProjectile
     {
+        public override void SetDefaults()
+        {
+            projectile.name = "Musical Sound";
+            projectile.width = 30;
+            projectile.height = 30;
+            projectile.alpha = 255;
+            projectile.timeLeft = 600;
+            projectile.penetrate = -1;
+            projectile.hostile = true;
+            projectile.magic = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+        }
+
+        public override void AI()
+        {
+        }
+
+        public override bool CanHitPlayer(Player target, ref int cooldownSlot)
+        {
+            return true;
+        }
+
+        public override void OnHitPlayer(Player target, int damage, bool crit)
+        {
+        }
     }
 }
