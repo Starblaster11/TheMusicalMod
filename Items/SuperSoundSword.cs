d86e2a0fa12fd192fdf9805e608b177dd9386141
Starblaster11 <adamgdarling@gmail.com>
2015-10-14 19:59:03 -0600

    Sword and sprite

---
+++ Items/SuperSoundSword.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TheMusicalMod.Items
+{
+    public class SuperSoundSword : ModItem
+    {
+    }
+}
