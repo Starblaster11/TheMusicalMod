2ebad5ccbe4d0e0da1c44e5e1b3ee8f2cd9ae467
Starblaster11 <adamgdarling@gmail.com>
2015-10-14 18:29:01 -0600

    Ok.

--- TheMusicalMod.cs
+++ TheMusicalMod.cs
@@ -3,6 +3,8 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+//using TheMusicalMod.Items; These will come in once we figure out what we will put in these folders.
+//using TheMusicalMod.NPCs;  These will come in once we figure out what we will put in these folders.
 
 namespace TheMusicalMod
 {
