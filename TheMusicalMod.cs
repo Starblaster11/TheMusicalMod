df9b3d9f3ed5f2d6f06989d5b78473ba9d322b06
Starblaster11 <adamgdarling@gmail.com>
2015-10-14 18:23:57 -0600

    I need this

---
+++ TheMusicalMod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheMusicalMod
+{
+    public class TheMusicalMod : Mod
+    {
+        public override void SetModInfo(out string name, ref ModProperties properties)
+        {
+            name = "TheExampleMod";
+            properties.Autoload = true;
+        }
+    }
+}
