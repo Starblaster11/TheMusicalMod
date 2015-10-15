33d4a62f240fc644e19234e18bd74fbf72137268
Starblaster11 <adamgdarling@gmail.com>
2015-10-14 19:39:26 -0600

    .

--- Projectiles/MusicalSound.cs
+++ Projectiles/MusicalSound.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
 
 namespace TheMusicalMod.Projectiles
 {
-    class MusicalSound
+    public class MusicalSound : ModProjectile
     {
+        public override void SetDefaults()
+        {
+            
+        }
     }
 }
